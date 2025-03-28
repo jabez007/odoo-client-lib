4f13b756e1b5c9e5867e063e80ffe06bcf4d95e6
jabez007 <jabez007@users.noreply.github.com>
2025-03-28 13:21:01 -0500

    clean up __init__

--- odoolib/connector/json_rpcs.py
+++ odoolib/connector/json_rpcs.py
@@ -11,13 +11,13 @@ class JsonRpcsConnector(JsonRpcConnector):
 
     PROTOCOL = "jsonrpcs"
 
-    def __init__(self, hostname, port=8069, version: Optional[str] = "2"):
+    def __init__(self, hostname: str, port=8069, version: Optional[str] = "2"):
         """
         Initialize by specifying the hostname and the port.
         :param hostname: The hostname of the computer holding the instance of Odoo.
         :param port: The port used by the Odoo instance for JsonRPC (default to 8069).
         """
-        super(JsonRpcsConnector, self).__init__(hostname, port)
+        super(JsonRpcsConnector, self).__init__(hostname, port, version)
         self._logger = logging.getLogger(
             f"{str.join('.', self._logger.name.split('.')[:-1])}.jsonrpcs"
         )
